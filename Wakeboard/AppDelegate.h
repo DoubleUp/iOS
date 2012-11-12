22790210615712314f9b881cb1648b5e9a49fd75
Chad Zeluff <czeluff@gmail.com>
2012-11-11 19:22:00 -0700

    initial commit

---
+++ Wakeboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wakeboard
+//
+//  Created by Chad Zeluff on 11/11/12.
+//  Copyright (c) 2012 Chad Zeluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Wakeboard/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Wakeboard
+//
+//  Created by Chad Zeluff on 11/11/12.
+//  Copyright (c) 2012 Chad Zeluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
